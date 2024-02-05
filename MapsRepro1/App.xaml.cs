1d2b72f0814150565ebe253fc6da2f3796544349
alikoli <aleksander.poniatowski@eje.pl>
2024-02-05 17:55:09 +0100

    Add project files.

---
+++ MapsRepro1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MapsRepro1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MapsRepro1/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MapsRepro1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
